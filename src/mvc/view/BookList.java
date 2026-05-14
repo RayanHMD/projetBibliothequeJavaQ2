35aa771a5210269050d6a0a295194f1a8adcbdde
RobinStrale <128152607+RobinStrale@users.noreply.github.com>
2026-05-14 16:07:55 +0200

    Interface listes des livres

--- src/mvc/view/BookList.java
+++ src/mvc/view/BookList.java
@@ -1,26 +1,51 @@
 package mvc.view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 public class BookList extends JPanel {
-    private JList<String> bookList;
-    private DefaultListModel<String> listModel;
+    private JTable table;
+    private DefaultTableModel tableModel;
+    private JComboBox<String> categoryBox;
+
 
     public BookList() {
         setLayout(new BorderLayout());
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.Y_AXIS));
 
+        //title
         JLabel title = new JLabel("Liste des livres", SwingConstants.CENTER);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-        listModel = new DefaultListModel<>();
 
-        listModel.addElement("Harry Potter");
-        listModel.addElement("Le Seigneur des Anneaux");
-        listModel.addElement("1984");
+        //panel category
+        JPanel panelCategory = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        categoryBox = new JComboBox<>();
 
-        bookList = new JList<>(listModel);
+        // add category in db
+        categoryBox.addItem("Toutes les catégories");
+        categoryBox.addItem("Fantasy");
+        categoryBox.addItem("Dystopie");
 
-        add(title, BorderLayout.NORTH);
-        add(new JScrollPane(bookList), BorderLayout.CENTER);
+        panelCategory.add(new Label("Catégorie : "));
+        panelCategory.add(categoryBox);
+
+        header.add(title);
+        header.add(panelCategory);
+        add(header, BorderLayout.NORTH);
+
+        // table
+        String[] columns = {"Code ISBN", "Titre du livre", "Maison d'édition", "Catégorie"};
+
+        tableModel = new DefaultTableModel(columns, 0);
+
+
+        // add books in db
+        tableModel.addRow(new Object[]{"978-123", "Harry Potter", "Gallimard", "Fantasy"});
+        tableModel.addRow(new Object[]{"978-456", "1984", "Seuil", "Dystopie"});
+        table = new JTable(tableModel);
+        add(new JScrollPane(table), BorderLayout.CENTER);
     }
 }
