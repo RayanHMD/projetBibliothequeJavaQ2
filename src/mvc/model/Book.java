a49408da789972d206832f8660185e5a4a93abd1
RayanHMD <etu57246@henallux.be>
2026-05-16 09:28:34 +0200

    Correction int isbn to String isbn dans Book

--- src/mvc/model/Book.java
+++ src/mvc/model/Book.java
@@ -1,19 +1,19 @@
 package mvc.model;
 
 public class Book {
-    private Integer isbn;
+    private String isbn;
     private String title;
     private Publisher publisher;
     private Category category;
 
-    public Book(Integer isbn, String title, Publisher publisher, Category category) {
+    public Book(String isbn, String title, Publisher publisher, Category category) {
         this.isbn = isbn;
         this.title = title;
         this.publisher = publisher;
         this.category = category;
     }
 
-    public Book(Integer isbn, String title, Category category) {
+    public Book(String isbn, String title, Category category) {
         this(isbn, title, null, category);
     }
 }
